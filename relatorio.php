e53ecb8158282d10c562b5d15ab5617b8baf3b87
Jorge Luis <jorgefloriano1646@gmail.com>
2024-02-25 21:49:21 -0300

    R. I. CMK

---
+++ relatorio.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Relatorio de Inspeção</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <div class="w3-col" style="width:25%;">
+                <img src="logo_cmk.jpg" alt="logo cmk" width="42%">
+            </div>
+            <div id="instr" class="w3-col" style="width:40%;">
+                <div>INSTRUÇÃO DE INSPEÇÃO</div>
+                <div>TALHA ELÉTRICA DE CORRENTE</div>
+            </div>
+            <div id="RI" class="w3-rest">RI Nº 000</div>
+        </div>
+    </header>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Cliente: CNH</th>
+                <th>Unidade: Sorocaba</th>
+                <th>Solicitante: Lucas Moraes</th>
+                <th>Nº Serie: 0909090</th>
+                <th>Nº Cliente: CNH-171</th>
+                <th>Nº CMK: 54</th>
+            </tr>
+            <tr>
+                <th>Fabricante: Demag</th>
+                <th>Modelo: DC-Pro 5</th>
+                <th>Capacidade: 500 kg</th>
+                <th>Prédio: P-80</th>
+                <th>Setor: Montagem</th>
+                <th>Área : Feeder</th>
+            </tr>
+        </thead>
+    </table>
+    <table style="margin-top: 10px;" class="table">
+        <thead>
+            <tr>
+                <th colspan="2" style="width: 50%;">INSPEÇÃO MECÂNICA</th>
+                <th colspan="2">INSPEÇÃO ELÉTRICA</th>
+            </tr>
+            <tr>
+                <th>TROLE</th>
+                <th style="width: 8%;">CAP:</th>
+                <th>TALHA</th>
+                <th style="width: 8%;"></th>
+            </tr>
+        </thead>
+        <tr>
+            <td>Rodas e rolamentos</td>
+            <td style="width: 8%;">Ok</td>
+            <td>Fixação do painel, tampa e limpeza</td>
+            <td style="width: 8%;">Ok</td>
+        </tr>
+    </table>
+</body>
+</html>
