c72d02ef471c546b1bf85f14402817b475793417
Jorge Luis <jorgefloriano1646@gmail.com>
2024-02-26 01:43:26 -0300

    1

--- relatorio.php
+++ relatorio.php
@@ -24,42 +24,68 @@
     <table class="table">
         <thead>
             <tr>
-                <th>Cliente: CNH</th>
-                <th>Unidade: Sorocaba</th>
-                <th>Solicitante: Lucas Moraes</th>
-                <th>Nº Serie: 0909090</th>
-                <th>Nº Cliente: CNH-171</th>
-                <th>Nº CMK: 54</th>
+                <th>Cliente: CNH</th><th>Unidade: Sorocaba</th><th>Solicitante: Lucas Moraes</th><th>Nº Serie: 0909090</th><th>Nº Cliente: CNH-171</th><th>Nº CMK: 54</th>
             </tr>
             <tr>
-                <th>Fabricante: Demag</th>
-                <th>Modelo: DC-Pro 5</th>
-                <th>Capacidade: 500 kg</th>
-                <th>Prédio: P-80</th>
-                <th>Setor: Montagem</th>
-                <th>Área : Feeder</th>
+                <th>Fabricante: Demag</th><th>Modelo: DC-Pro 5</th><th>Capacidade: 500 kg</th><th>Prédio: P-80</th><th>Setor: Montagem</th><th>Área : Feeder</th>
             </tr>
         </thead>
     </table>
     <table style="margin-top: 10px;" class="table">
         <thead>
-            <tr>
-                <th colspan="2" style="width: 50%;">INSPEÇÃO MECÂNICA</th>
-                <th colspan="2">INSPEÇÃO ELÉTRICA</th>
-            </tr>
-            <tr>
-                <th>TROLE</th>
-                <th style="width: 8%;">CAP:</th>
-                <th>TALHA</th>
-                <th style="width: 8%;"></th>
-            </tr>
+            <tr><th colspan="3" style="width: 50%;">INSPEÇÃO MECÂNICA</th><th colspan="3">INSPEÇÃO ELÉTRICA</th></tr>
+            <tr><th colspan="2">TROLE</th><th style="width: 10%;">Cap:500 kg</th><th colspan="2">TALHA</th><th style="width: 10%;"></th></tr>
         </thead>
-        <tr>
-            <td>Rodas e rolamentos</td>
-            <td style="width: 8%;">Ok</td>
-            <td>Fixação do painel, tampa e limpeza</td>
-            <td style="width: 8%;">Ok</td>
-        </tr>
+        <tr><td style="width: 3%;">01</td><td>Rodas e rolamentos</td><td>Ok</td><td style="width: 3%;">36</td><td>Fixação do painel, tampa e limpeza</td><td>Recuperar</td></tr>
+        <tr><td>02</td><td>Fixação da talha e parafusos de fechamento</td><td>Ok</td><td>37</td><td>Chave geral, fusíveis e disjuntores</td><td>Ok</td></tr>
+        <tr><td>03</td><td>Batentes</td><td>Ok</td><td>38</td><td>Motores (medições e aquecimento)</td><td>Ok</td></tr>
+        <tr><td>04</td><td>Motor (fixação e ruidos anormais)</td><td>Ok</td><td>39</td><td>Caixa de ligação</td><td>Ok</td></tr>
+        <tr><td>05</td><td>Freio (regular se necessário)</td><td>Ok</td><td>40</td><td>Cabo de alimentação</td><td>Ok</td></tr>
+        <tr><td>06</td><td>Redutor</td><td>Ok</td><td>41</td><td>Prensa cabos</td><td>Ok</td></tr>
+        <tr><th colspan="2">TROLE</th><th style="width: 10%;">Cap:500 kg</th><td>42</td><td>Tomadas de engate rápido</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>43</td><td>Contatores e contatos</td><td>Trocar</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>44</td><td>Trafos de potência e comando</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>45</td><td>Bornes e terminais</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>46</td><td>Banco de resistência</td><td>Não insp.</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>47</td><td>Célula de carga</td><td>Não aplica</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><th colspan="2">Botoeira</th><th style="width: 10%;"></th></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
+        <tr><td>1</td><td>Rodas e rolamentos</td><td>Ok</td><td>1</td><td>Fixação do painel, tampa e limpeza</td><td>Ok</td></tr>
     </table>
 </body>
 </html>
